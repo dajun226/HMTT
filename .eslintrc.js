4610a96846479ce187a4d7e1c671893a7729b2e9
senyuxi <dajun226@163.com>
2022-07-03 11:54:22 +0800

    更新目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    // 解决打开文件 eslint第一行报错问题
+    requireConfigFile: false
   },
   rules: {
+    'space-before-function-paren': 'off',
+    // 解决eslint某行报错
+    'vue/no-mutating-props': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-expressions': 'off',
+    'no-sequences': 'off',
+    'vue/no-parsing-error': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 6666,
+      host: '127.0.0.1'
+    }
+  }
 })
